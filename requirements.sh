51ae695513374b7ca4d0b6e1c02bca9c7630eb9e
habelct <60401440+habelct@users.noreply.github.com>
2021-01-02 14:54:48 +0700

    Create requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,10 @@
+sudo apt-get install python3
+sudo apt install python3-pip
+
+pip3 install opencv-python
+pip3 install scikit-build
+python3 -m pip install --upgrade pip
+
+chmod 777 kernelburger.sh
+chmod 777 cam.py
+chmod 777 ogcam.py
